bd84f4a104e5420c5bfa14d465eaea6b45018b16
Jens Abraham <Jens_Abraham@gmx.de>
2010-09-28 21:09:46 +0200

    timetracker: data storage with json implemented

--- timetracker.py
+++ timetracker.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python
 
-DB = ~/.timetracker/db.json
+import datetime
+import json
+import os
+import os.path
+
+DB = os.environ['HOME'] + "/.timetracker/db.json"
+DATA = []
+
+def add(argv):
+    global DATA
+    print "adding entry"
+    entry = {'tag' : "testing", 
+             'begin' : datetime.datetime.today().isoformat(' '),
+             'end' : "end", 
+             'comment' : "comment" }
+
+    DATA.append(entry)
+
+def show(argv):
+    print "show entrys"
+
+def delete(argv):
+    print "delete entry"
+
+def help(argv):
+    print "timetracer - by core - 2010"
+    print
+    print "pre alpha - more coming soon"
+
+def load_json(io):
+    global DATA 
+    DATA = json.load(io)
+
+def save_json(io):
+    json.dump(DATA, io)
 
 def main(argv):
     actions = {
@@ -13,7 +47,16 @@ def main(argv):
     if len(argv):
         action_id = argv[0]
         if action_id in actions:
+            if os.path.exists(DB):
+                io = open(DB, 'r')
+                load_json(io)
+                io.close()
+
             actions[action_id](argv[1:])
+
+            io = open(DB, 'w')
+            save_json(io)
+            io.close()
         else:
             print "Unknown command, use one of those:"
 
